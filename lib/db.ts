ff1244653d82c72a0f0cfc7dd5bdd859c16f580f
Juan Ruben Hernandez Gonzalez <ts250mx@gmail.com>
2026-05-01 19:55:29 -0600

    refactor: revert dev script and add fallback values to db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'integramembers.com',
+  user: process.env.DB_USER || 'kyk',
+  password: process.env.DB_PASSWORD || 'merkurio',
+  database: process.env.DB_NAME || 'HP_Tapioki',
   port: parseInt(process.env.DB_PORT || '3306'),
   waitForConnections: true,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '10'),
