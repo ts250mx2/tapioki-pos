3c27814db417b621d7281400f3911200cfc0a593
Juan Ruben Hernandez Gonzalez <ts250mx@gmail.com>
2026-05-01 19:07:13 -0600

    fix: use absolute path for outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  outputFileTracingRoot: process.cwd(),
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
